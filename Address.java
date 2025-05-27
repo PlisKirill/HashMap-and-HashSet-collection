8ebd45020d414b99b13cc7d5f1f5ba48ca87b2f2
PlisKirill <lirik0503@yandex.ru>
2025-05-27 18:50:17 +0300

    Add files via upload

--- Address.java
+++ Address.java
@@ -25,4 +25,14 @@ public class Address {
         return city;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(country, city);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Address a = (Address) obj;
+        return country.equals(a.country) && city.equals(a.city);
+    }
 }
